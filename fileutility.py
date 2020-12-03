49ffcf6b0c9344f54e53633f63064318a510e3fa
Thijs <thijs.kooyers@gmail.com>
2020-12-03 09:23:24 +0100

    completed day 2

--- fileutility.py
+++ fileutility.py
@@ -8,10 +8,18 @@ def get_execution_path():
 def get_resource_path(filename):
     return get_execution_path() + "\\resources\\" + filename
 
-def read_numbers_file(fileName):
-    filePath = get_resource_path(fileName);
-    file = open(filePath, mode='r')
+
+def read_lines_file(filename):
+    file_path = get_resource_path(filename);
+    file = open(file_path, mode='r')
     lines = file.readlines()
+    for i, line in enumerate(lines):
+        lines[i] = line.replace("\n", "")
     file.close()
+    return lines
+
+
+def read_numbers_file(filename):
+    lines = read_lines_file(filename)
     return list(map(int, lines))
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import dayone
+import daytwo
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    dayone.solve()
+    daytwo.solve()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
