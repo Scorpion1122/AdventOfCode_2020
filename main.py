4a1f26376e27f933a40203480da7240a3749b8a1
Thijs <thijs.kooyers@gmail.com>
2020-12-06 08:53:58 +0100

    solution day 6

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import dayfive
+import daysix
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    dayfive.solve()
+    daysix.solve()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
